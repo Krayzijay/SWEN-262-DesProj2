a33d2e28d9a99c464023a79bbe0f810ffcc1cb68
JP <john.dumont11@gmail.com>
2022-03-09 17:45:39 -0500

    started, needs to execute strategy methods

--- src/SearchState.java
+++ src/SearchState.java
@@ -3,7 +3,16 @@ package src;
 public class SearchState implements State{
 
     @Override
-    public void execute() {
+    public void execute(Library db, String[] tokens){
+        String[] keyWords = tokens[0].split(" ");
+        //Search Song ByTitle 'Mr. Blue Sky
+        String item = keyWords[1];
+        String searchBy = keyWords[2];
+        String param = tokens[1];
 
+        to do list:
+        setstrategy()
+        execute()
+        post data
     }
 }
