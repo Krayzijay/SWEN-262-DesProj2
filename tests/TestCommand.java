38d45bcf27da1d9da24c6551168d9e732174dbfd
Jackson Murphy <69988863+jmm5915@users.noreply.github.com>
2022-03-11 21:58:45 -0500

    Tested Remove release and song

--- tests/TestCommand.java
+++ tests/TestCommand.java
@@ -17,6 +17,8 @@ import src.Database.Song;
 import src.LibraryCommands.AddReleaseAction;
 import src.LibraryCommands.AddSongAction;
 import src.LibraryCommands.RateSongAction;
+import src.LibraryCommands.RemoveReleaseAction;
+import src.LibraryCommands.RemoveSongAction;
 
 @Testable
 public class TestCommand {
@@ -83,6 +85,49 @@ public class TestCommand {
 
         // test
         assertEquals(song.getUserRating(), personal.getSongs().get(0).getUserRating(), "Testing if the song has been rated by the user");
+    }
+
+    /**
+     * Tests that a release has been remove from the personal collection
+     * @throws IOException
+     */
+    @Test
+    public void testRemoveReleaseAction() throws IOException {
+        //setup
+        Library personal = new Library();
+        Library global = new Library();
+        Artist artist = new Artist("GUID", "artist");
+        ArrayList<Song> songs = new ArrayList<>() {{add(new Song("GUID", artist, 1, "title"));}};
+        Release expected = new Release("1234", artist, "a release", "medium", "01/01/2001", songs);
+        personal.addRelease(expected);
+
+        // actual
+        RemoveReleaseAction action = new RemoveReleaseAction(global, personal);
+        action.performAction("a release", "01/01/2001");
+
+        // test
+        assertEquals(0, personal.getReleases().size(), "Testing if the release has been removed from the user's collection");
+    }
+
+    /**
+     * Tests that a song has been remove from the personal collection
+     * @throws IOException
+     */
+    @Test
+    public void testRemoveSongAction() throws IOException {
+        //setup
+        Library personal = new Library();
+        Library global = new Library();
+        Artist artist = new Artist("GUID", "artist");
+        Song song = new Song("GUID", artist, 1, "title");
+        personal.addSong(song);
+
+        // actual
+        RemoveSongAction action = new RemoveSongAction(global, personal);
+        action.performAction("title");
+
+        // test
+        assertEquals(0, personal.getSongs().size(), "Testing if the song has been removed from the user's collection");
     }
 
 }
